862a13d77f695ddb711c2c5b6b7536578a0e21ee
Layal Singh <layal.singhkolkata@gmail.com>
2024-08-29 15:51:46 -0400

    timers,start,reset added

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
 const task = document.getElementById('task');
 const tasklist = document.getElementById('task-list');
-const workTime = document.getElementById('work-time');
-const shortBreak = document.getElementById('short');
-const longBreak = document.getElementById('long');
+var time = document.getElementById('time-left');
+var startPause = document.getElementById('start');
+var stop = false;
+var pause = false;
 
-
+console.log(stop);
 function addTask() {
     if (task.value === '') {
         alert('Please enter a task');
@@ -17,3 +18,43 @@ function addTask() {
     }
 }
 
+function setTimer(newTime) {
+    time.innerHTML = newTime;
+    time.innerHTML = newTime + ':00';
+    clearInterval(time);
+}
+
+function Timer() {
+    var timeArray = time.innerHTML.split(':');
+    var minutes = parseInt(timeArray[0]);
+    var seconds = parseInt(timeArray[1]);
+    var audio = new Audio('sounds/microwave-timer-117077.mp3');
+    var prevTime = time.innerHTML;
+
+    startPause.innerHTML = 'Pause';
+    var countdown = setInterval(function() {
+        if (stop) {
+            clearInterval(countdown);
+            stop = false;
+            time.innerHTML = prevTime;
+            startPause.innerHTML = 'Start';
+            return;
+        }
+        if (minutes === 0 && seconds === 0) {
+            clearInterval(countdown);
+            audio.play();
+            startPause.innerHTML = 'Start';
+            time.innerHTML = prevTime;
+        } else if (seconds === 0) {
+            minutes--;
+            seconds = 59;
+        } else {
+            seconds--;
+        }
+        time.innerHTML = minutes + ':' + seconds;
+    }, 1000);
+}
+
+function resetTimer() {
+    stop = true;
+}
