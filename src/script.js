71ce9b256e60241d62945ebe638a4a5892e1fb09
Layal Singh <layal.singhkolkata@gmail.com>
2024-08-30 00:23:25 -0400

    timer functionality

--- src/script.js
+++ src/script.js
@@ -1,11 +1,16 @@
 const task = document.getElementById('task');
 const tasklist = document.getElementById('task-list');
 var time = document.getElementById('time-left');
-var startPause = document.getElementById('start');
+var start = document.getElementById('start');
+const work = document.getElementById('work');
+const shortBreak = document.getElementById('short');
+const longBreak = document.getElementById('long');
 var stop = false;
 var pause = false;
-
-console.log(stop);
+var change = false;
+var prevTime = time.innerHTML;
+var ongoing = false;
+    
 function addTask() {
     if (task.value === '') {
         alert('Please enter a task');
@@ -19,9 +24,12 @@ function addTask() {
 }
 
 function setTimer(newTime) {
-    time.innerHTML = newTime;
+    if (ongoing) {
+        change = true;
+    }
     time.innerHTML = newTime + ':00';
-    clearInterval(time);
+    prevTime = newTime + ':00';
+    
 }
 
 function Timer() {
@@ -29,21 +37,36 @@ function Timer() {
     var minutes = parseInt(timeArray[0]);
     var seconds = parseInt(timeArray[1]);
     var audio = new Audio('sounds/microwave-timer-117077.mp3');
-    var prevTime = time.innerHTML;
-
-    startPause.innerHTML = 'Pause';
+    ongoing = true;
+    console.log(ongoing);
     var countdown = setInterval(function() {
+        if (pause) {
+            clearInterval(countdown);
+            pause = false;
+            ongoing = false;
+            time.innerHTML = minutes + ':' + seconds;
+            console.log('pause',pause);
+            return;
+            
+        }
+        if (change) {
+            clearInterval(countdown);
+            change = false;
+            ongoing = false;
+            console.log('change',change);
+            return;
+        }
         if (stop) {
             clearInterval(countdown);
             stop = false;
-            time.innerHTML = prevTime;
-            startPause.innerHTML = 'Start';
+            ongoing = false;
+            console.log('stop',stop);
             return;
         }
         if (minutes === 0 && seconds === 0) {
             clearInterval(countdown);
             audio.play();
-            startPause.innerHTML = 'Start';
+            start.innerHTML = 'Start';
             time.innerHTML = prevTime;
         } else if (seconds === 0) {
             minutes--;
@@ -57,4 +80,14 @@ function Timer() {
 
 function resetTimer() {
     stop = true;
+    time.innerHTML = prevTime;
+
+    // stop = false;
+    console.log(prevTime);
+}
+
+function pauseTimer() {
+    if (ongoing) {
+        pause = true;
+    }
 }
